222c13afb55254006dc07d879b09e2f40db4262c
Waleed AlMalki <wm.wexem@gmail.com>
2019-03-01 16:29:11 +0300

    Create Modal.js

---
+++ Modal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Animated, LayoutAnimation, TouchableOpacity, StyleSheet, Text, View, Platform } from 'react-native';
+
+export class Modal extends React.Component {
+  
+}
+
+export class ModalContainer extends React.Component {
+
+}
