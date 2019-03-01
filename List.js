dbf04e9c4911b7824e46c66e3fba396c41156080
Waleed AlMalki <wm.wexem@gmail.com>
2019-03-01 16:29:07 +0300

    Update List.js

--- List.js
+++ List.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { ScrollView, FlatList, StyleSheet, Text, View } from 'react-native';
+import { Animated, ScrollView, FlatList, StyleSheet, Text, View } from 'react-native';
 
 import { ShadowOverlay } from './ShadowOverlay';
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
 export class List extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      scrollAnimation: new Animated.Value(0),
       reachedStart: true,
       reachedEnd: false,
+      topOverScrollHeight: 0,
     }
 
     this.contentSize = 0
@@ -20,19 +24,19 @@ export class List extends React.Component {
   }
   scrollToIndex(i) {
     if(i==this.props.data.length-1){
-      this.flatListRef.scrollToEnd({animated: true})
+      this.flatListRef.getNode().scrollToEnd({animated: true})
     }else{
-      this.flatListRef.scrollToIndex({index: i, viewPosition: .5, animated: true})
+      this.flatListRef.getNode().scrollToIndex({index: i, viewPosition: .5, animated: true})
     }
   }
   scrollTo(v) {
     if(this.flatListRef){
-      this.flatListRef.scrollToOffset(v)
+      this.flatListRef.getNode().scrollToOffset(v)
     }
   }
   scrollToEnd(v) {
     if(this.flatListRef){
-      this.flatListRef.scrollToEnd(v)
+      this.flatListRef.getNode().scrollToEnd(v)
     }
   }
   handleScroll(e) {
@@ -40,7 +44,9 @@ export class List extends React.Component {
     const endOffset = this.props.endOffset?this.contentSize-this.size+(this.props.startOffset?this.props.startOffset:0)+this.props.endOffset:this.contentSize-this.size
 
     const contentOffset = !this.props.horizontal?e.nativeEvent.contentOffset.y:e.nativeEvent.contentOffset.x
-
+    if(contentOffset<startOffset&&this.props.topOverScrollColor){
+      this.setState({topOverScrollHeight: 20+Math.abs(contentOffset)})
+    }
     if(contentOffset<=startOffset&&!this.state.reachedStart&&!this.props.noStartOverlay){
       this.setState({reachedStart: true})
       this.startShadowOverlay.hide()
@@ -72,15 +78,28 @@ export class List extends React.Component {
         onLayout={(e)=>{
           this.size = !this.props.horizontal?e.nativeEvent.layout.height:e.nativeEvent.layout.width
         }}>
+        {this.props.topOverScrollColor?(
+          <View style={[styles.topOverScroll, {backgroundColor: this.props.topOverScrollColor, height: this.state.topOverScrollHeight}]}></View>
+        ):null}
         {this.props.data.length>0?(
-        <FlatList
+        <AnimatedFlatList
           style={{overflow: this.props.overflowVisible?'visible':'hidden'}}
           contentContainerStyle={[!this.props.horizontal?{paddingTop: this.props.startOffset?this.props.startOffset:0, paddingBottom: this.props.endOffset?this.props.endOffset:0}:{paddingLeft: this.props.startOffset?this.props.startOffset:0, paddingRight: this.props.endOffset?this.props.endOffset:0}, {overflow: this.props.overflowVisible?'visible':'hidden'}]}
           nestedScrollEnabled={true}
           onContentSizeChange={(contentWidth, contentHeight)=>{
             this.contentSize = !this.props.horizontal?contentHeight:contentWidth
           }}
-          onScroll={this.handleScroll}
+          onScroll={Animated.event(
+        [{
+            nativeEvent: {
+                contentOffset: {
+                    y: null
+                }
+            }
+        }],{
+          useNativeDriver: true,
+          listener: this.handleScroll,
+        })}
           scrollEventThrottle={1}
           ref={(ref) => {this.flatListRef = ref}}
           {...props}/>
@@ -95,7 +114,7 @@ export class List extends React.Component {
         <ShadowOverlay ref={(ref)=>{this.startShadowOverlay=ref}} initiallyShown={!this.state.reachedStart&&this.props.data.length>0} horizontal={this.props.horizontal} color={this.props.overlayColor} size={this.props.overlaySize} />
         ):null}
         {!this.props.noEndOverlay?(
-        <ShadowOverlay ref={(ref)=>{this.endShadowOverlay=ref}} initiallyShown={!this.state.reachedEnd&&this.props.data.length>0} horizontal={this.props.horizontal} last={true} color={this.props.overlayColor} size={this.props.overlaySize} />
+        <ShadowOverlay ref={(ref)=>{this.endShadowOverlay=ref}} initiallyShown={!this.state.reachedEnd} horizontal={this.props.horizontal} last={true} color={this.props.overlayColor} size={this.props.overlaySize} />
         ):null}
       </View>
     )
@@ -107,5 +126,10 @@ const styles = StyleSheet.create({
     fontFamily: 'pt-mono-bold',
     fontSize: 14,
     textAlign: 'center',
+  },
+  topOverScroll: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
   }
 });
